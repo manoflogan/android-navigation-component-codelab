95a7c2b4a67fc5de730badac6436c45246a17848
manoflogan <manoflogan@gmail.com>
2021-04-11 18:14:59 -0700

    First pass of navigation component

---
+++ app/src/main/java/com/krishnanand/navigationcomponeent/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.krishnanand.navigationcomponeent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+}
