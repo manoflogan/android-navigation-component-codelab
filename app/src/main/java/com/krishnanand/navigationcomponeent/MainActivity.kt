90205a714997adf2efe68f2bb5f77df826bd4c90
manoflogan <manoflogan@gmail.com>
2021-04-28 22:42:30 -0700

    Custom toolbar and local, and global navigation action.

--- app/src/main/java/com/krishnanand/navigationcomponeent/MainActivity.kt
+++ app/src/main/java/com/krishnanand/navigationcomponeent/MainActivity.kt
@@ -1,18 +1,53 @@
 package com.krishnanand.navigationcomponeent
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
-    val toolbar: Toolbar by lazy {
+    private val toolbar: Toolbar by lazy {
         findViewById(R.id.toolbar)
     }
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as  NavHostFragment
+        navController = navHostFragment.findNavController()
         setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp() || super.onSupportNavigateUp()
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.termsAndConditions -> {
+                with(NavGraphDirections.actionGlobalTermsAndConditionsFragment()) {
+                    navController.navigate(this)
+                }
+                true
+            }
+            else ->  item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
+
     }
 }
---
+++ app/src/main/java/com/krishnanand/navigationcomponeent/TermsAndConditionsFragment.kt
@@ -0,0 +1,5 @@
+package com.krishnanand.navigationcomponeent
+
+import androidx.fragment.app.Fragment
+
+class TermsAndConditionsFragment: Fragment(R.layout.fragment_terms)
