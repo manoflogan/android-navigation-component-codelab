3c5b0f275eb984c3272c05a1fd22e09b6ec7d1c7
manoflogan <manoflogan@gmail.com>
2021-05-02 15:34:56 -0700

    Part 9 Deep link set up.

--- app/src/main/java/com/krishnanand/navigationcomponeent/LoginFragment.kt
+++ app/src/main/java/com/krishnanand/navigationcomponeent/LoginFragment.kt
@@ -5,13 +5,16 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
 
+    val args: LoginFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        edit_username.setText(args.username)
         button_confirm.setOnClickListener {
             val username = edit_username.text.toString()
             val password = edit_password.text.toString()
